aea7c3a600c7bc7639cd14c6d678eaf7960e07d4
adityasankhla01 <93070213+adityasankhla01@users.noreply.github.com>
2022-12-24 17:31:04 +0530

    Image container picture pickup corrected

--- lib/presentation/posts/post_form/widgets/image_field_widget.dart
+++ lib/presentation/posts/post_form/widgets/image_field_widget.dart
@@ -1,12 +1,21 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:didkyo/application/posts/post_form/post_form_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImageField extends StatelessWidget {
+class ImageField extends StatefulWidget {
+  @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
   String postImage = '';
+
+  String imageWay = '';
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -26,21 +35,56 @@ class ImageField extends StatelessWidget {
               width: size.width,
               height: size.height / 4,
               child: postImage == ""
-                  ? Center(
-                      child: IconButton(
-                        icon: const Icon(Icons.add_box_rounded),
-                        onPressed: () async {
-                          ImagePicker picker = ImagePicker();
-                          final XFile? image = await picker.pickImage(
-                              source: ImageSource.gallery);
-                          if (image != null) {
-                            context
-                                .bloc<PostFormBloc>()
-                                .add(PostFormEvent.imageChanged(image.path));
-                          }
-                        },
-                      ),
-                    )
+                  ? imageWay == ""
+                      ? Center(
+                          child: IconButton(
+                          icon: const Icon(Icons.add_box_rounded),
+                          onPressed: () async {
+                            ImagePicker picker = ImagePicker();
+                            final XFile? image = await picker.pickImage(
+                                source: ImageSource.gallery);
+                            if (image != null) {
+                              setState(() {
+                                imageWay = image.path;
+                              });
+
+                              context
+                                  .bloc<PostFormBloc>()
+                                  .add(PostFormEvent.imageChanged(image.path));
+                            }
+                          },
+                        ))
+                      : Stack(
+                          children: [
+                            Positioned.fill(
+                                child: Image.file(
+                              File(imageWay),
+                              fit: BoxFit.cover,
+                            )),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: IconButton(
+                                onPressed: () async {
+                                  ImagePicker picker = ImagePicker();
+                                  final XFile? image = await picker.pickImage(
+                                      source: ImageSource.gallery);
+                                  if (image != null) {
+                                    setState(() {
+                                      imageWay = image.path;
+                                    });
+                                    context.bloc<PostFormBloc>().add(
+                                        PostFormEvent.imageChanged(image.path));
+                                    log(state.post.postImage.getOrCrash());
+                                  }
+                                },
+                                icon: const Icon(
+                                  Icons.add_box_rounded,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
                   : Stack(
                       children: [
                         Positioned.fill(
@@ -55,6 +99,9 @@ class ImageField extends StatelessWidget {
                               final XFile? image = await picker.pickImage(
                                   source: ImageSource.gallery);
                               if (image != null) {
+                                setState(() {
+                                  imageWay = image.path;
+                                });
                                 context.bloc<PostFormBloc>().add(
                                     PostFormEvent.imageChanged(image.path));
                                 log(state.post.postImage.getOrCrash());
--- lib/presentation/settings/widgets/userImageField.dart
+++ lib/presentation/settings/widgets/userImageField.dart
@@ -1,14 +1,27 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:didkyo/application/user_settings/user_settings_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
-class UserImageField extends StatelessWidget {
+class UserImageField extends StatefulWidget {
+  @override
+  State<UserImageField> createState() => _UserImageFieldState();
+}
+
+class _UserImageFieldState extends State<UserImageField> {
+  static const String loadingImage =
+      'https://cdn3.vectorstock.com/i/1000x1000/98/47/loading-icon-on-black-vector-24559847.jpg';
   String postImage = '';
+  String localImage = '';
+
+  String imageWay = '';
+
   @override
   Widget build(BuildContext context) {
+    log('post image is ' + postImage);
     Size size = MediaQuery.of(context).size;
     return BlocConsumer<UserSettingsBloc, UserSettingsState>(
         listener: (context, state) {
@@ -22,27 +35,61 @@ class UserImageField extends StatelessWidget {
           ),
           width: size.width,
           height: size.height / 4,
-          child: postImage == ""
-              ? Center(
-                  child: IconButton(
-                    icon: const Icon(Icons.add_box_rounded),
-                    onPressed: () async {
-                      ImagePicker picker = ImagePicker();
-                      final XFile? image =
-                          await picker.pickImage(source: ImageSource.gallery);
-                      if (image != null) {
-                        context
-                            .bloc<UserSettingsBloc>()
-                            .add(UserSettingsEvent.imageChanged(image.path));
-                      }
-                    },
-                  ),
-                )
+          child: localImage != ''
+              ? imageWay == ""
+                  ? Center(
+                      child: IconButton(
+                      icon: const Icon(Icons.add_box_rounded),
+                      onPressed: () async {
+                        ImagePicker picker = ImagePicker();
+                        final XFile? image =
+                            await picker.pickImage(source: ImageSource.gallery);
+                        if (image != null) {
+                          setState(() {
+                            imageWay = image.path;
+                          });
+
+                          context
+                              .bloc<UserSettingsBloc>()
+                              .add(UserSettingsEvent.imageChanged(image.path));
+                        }
+                      },
+                    ))
+                  : Stack(
+                      children: [
+                        Positioned.fill(
+                            child: Image.file(
+                          File(imageWay),
+                          fit: BoxFit.cover,
+                        )),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: IconButton(
+                            onPressed: () async {
+                              ImagePicker picker = ImagePicker();
+                              final XFile? image = await picker.pickImage(
+                                  source: ImageSource.gallery);
+                              if (image != null) {
+                                setState(() {
+                                  imageWay = image.path;
+                                });
+                                context.bloc<UserSettingsBloc>().add(
+                                    UserSettingsEvent.imageChanged(image.path));
+                              }
+                            },
+                            icon: const Icon(
+                              Icons.add_box_rounded,
+                              color: Colors.red,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
               : Stack(
                   children: [
                     Positioned.fill(
                         child: Image.network(
-                      postImage,
+                      postImage != "" ? postImage : loadingImage,
                     )),
                     Align(
                       alignment: Alignment.bottomRight,
@@ -52,9 +99,12 @@ class UserImageField extends StatelessWidget {
                           final XFile? image = await picker.pickImage(
                               source: ImageSource.gallery);
                           if (image != null) {
+                            setState(() {
+                              imageWay = image.path;
+                              localImage = 'NOT NULL ';
+                            });
                             context.bloc<UserSettingsBloc>().add(
                                 UserSettingsEvent.imageChanged(image.path));
-                            log(state.user.photoUrl!);
                           }
                         },
                         icon: const Icon(
