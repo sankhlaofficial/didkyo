9e9ef277f81406426b7d2edc28f7daacc30b49b4
adityasankhla01 <93070213+adityasankhla01@users.noreply.github.com>
2022-12-30 11:28:15 +0530

    Images added on trending cards

--- lib/presentation/trending/widgets/trending_card_grid.dart
+++ lib/presentation/trending/widgets/trending_card_grid.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:didkyo/application/posts/post_watcher/post_watcher_bloc.dart';
+import 'package:didkyo/injection.dart';
 import 'package:didkyo/presentation/global_widgets/shadow_container.dart';
 import 'package:didkyo/presentation/posts/location_posts/location_posts_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 class TrendingCardsGrid extends StatelessWidget {
@@ -17,6 +20,7 @@ class TrendingCardsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log(trendingData.keys.toList().length.toString());
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: GridView.builder(
@@ -24,41 +28,66 @@ class TrendingCardsGrid extends StatelessWidget {
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2),
           itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                log(trendingData.keys.toList()[index]);
-                Get.to(() => LocationPostsPage(
-                    selectedLocation: trendingData.keys.toList()[index]));
-              },
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10),
-                child: ShadowContainer(
-                  color: cardColors[0],
-                  child: Container(
-                    width: 40,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: cardColors[0],
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(color: Colors.black)),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            trendingData.keys.toList()[index],
-                            style: Theme.of(context).textTheme.displayMedium,
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text('${trendingData.values.toList()[index]} posts'),
-                        ],
-                      ),
-                    ),
-                  ),
+            return BlocProvider<PostWatcherBloc>(
+              create: (context) => getIt<PostWatcherBloc>()
+                ..add(
+                  PostWatcherEvent.watchLocationSpecificStarted(
+                      trendingData.keys.toList()[index]),
                 ),
+              child: BlocBuilder<PostWatcherBloc, PostWatcherState>(
+                builder: (context, state) {
+                  return state.map(
+                      initial: (_) => Container(),
+                      loadInProgress: (_) => Container(),
+                      loadSuccess: (state) {
+                        return InkWell(
+                          onTap: () {
+                            log(trendingData.keys.toList()[index]);
+                            Get.to(() => LocationPostsPage(
+                                selectedLocation:
+                                    trendingData.keys.toList()[index]));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15.0, horizontal: 10),
+                            child: ShadowContainer(
+                              color: Colors.transparent,
+                              child: Container(
+                                width: 40,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(state
+                                            .posts[0].postImage
+                                            .getOrCrash())),
+                                    borderRadius: BorderRadius.circular(12),
+                                    border: Border.all(color: Colors.black)),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        trendingData.keys.toList()[index],
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .displayMedium,
+                                      ),
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                          '${trendingData.values.toList()[index]} posts'),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      loadFailure: (_) => Container());
+                },
               ),
             );
           }),
