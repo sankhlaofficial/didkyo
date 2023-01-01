104d9264df658b6b1cb2d4cbc3daefecebce57fa
adityasankhla01 <93070213+adityasankhla01@users.noreply.github.com>
2023-01-01 18:04:51 +0530

    Minor bug fixes in UI

--- lib/presentation/profile/widgets/user_images_grid.dart
+++ lib/presentation/profile/widgets/user_images_grid.dart
@@ -25,43 +25,46 @@ class UserImagesGrid extends StatelessWidget {
               initial: (_) => Container(),
               loadInProgress: (_) => Container(),
               loadSuccess: (state) {
-                return GridView.builder(
-                  cacheExtent: 99999999,
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3),
-                    itemCount: state.posts.length,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          nav.Get.to(
-                              () => PostFullScreen(
-                                  currentUserId: user.id!.getOrCrash(),
-                                  clickedPost: state.posts[index]),
-                              transition: nav.Transition.downToUp);
-                        },
-                        child: CachedNetworkImage(
-                          imageUrl: state.posts[index].postImage.getOrCrash(),
-                          imageBuilder: (context, imageProvider) => Container(
-                            margin: const EdgeInsets.all(3),
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                image: DecorationImage(image: imageProvider)),
-                          ),
-                          placeholder: (context, url) => Container(
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 108.0),
+                  child: GridView.builder(
+                      cacheExtent: 99999999,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3),
+                      itemCount: state.posts.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            nav.Get.to(
+                                () => PostFullScreen(
+                                    currentUserId: user.id!.getOrCrash(),
+                                    clickedPost: state.posts[index]),
+                                transition: nav.Transition.downToUp);
+                          },
+                          child: CachedNetworkImage(
+                            imageUrl: state.posts[index].postImage.getOrCrash(),
+                            imageBuilder: (context, imageProvider) => Container(
                               margin: const EdgeInsets.all(3),
                               width: 100,
                               height: 100,
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(8),
-                                  color: Colors.grey)),
-                        ),
-                      );
-                    });
+                                  image: DecorationImage(image: imageProvider)),
+                            ),
+                            placeholder: (context, url) => Container(
+                                margin: const EdgeInsets.all(3),
+                                width: 100,
+                                height: 100,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.grey)),
+                          ),
+                        );
+                      }),
+                );
               },
               loadFailure: (_) => Container());
         },
